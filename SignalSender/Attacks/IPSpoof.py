a3fdc0dcdcdea9e0ef48903b8f1f9396db103d7e
dAninAd1248 <124212726+dAninAd1248@users.noreply.github.com>
2026-06-17 08:48:32 -0400

    Add IP spoofing function

---
+++ SignalSender/Attacks/IPSpoof.py
@@ -0,0 +1,14 @@
+from scapy.layers.inet import IP, UDP
+from scapy.packet import Raw
+
+
+def IPSpoof(packet, spoofed_ip="192.168.1.100"):
+    packet[IP].src = spoofed_ip
+    return packet
+
+
+if __name__ == "__main__":
+    packet = (IP(dst="127.0.0.1") / UDP(sport=4000, dport=5000) / Raw(load=b"Good morning, receiver :)"))
+
+    payload = IPSpoof(packet)
+    print(payload.show2())
