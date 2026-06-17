9ecc5be8bb4c5c3ce9d1aa60e283d795ea39c1b8
dAninAd1248 <124212726+dAninAd1248@users.noreply.github.com>
2026-06-17 15:57:56 -0400

    Show old packet

--- SignalSender/Attacks/IPSpoof.py
+++ SignalSender/Attacks/IPSpoof.py
@@ -9,6 +9,6 @@ def IPSpoof(packet, spoofed_ip="192.168.1.100"):
 
 if __name__ == "__main__":
     packet = (IP(dst="127.0.0.1") / UDP(sport=4000, dport=5000) / Raw(load=b"Good morning, receiver :)"))
-
-    payload = IPSpoof(packet)
-    print(payload.show2())
+    print(packet.show2())
+    packet = IPSpoof(packet)
+    print(packet.show2())
