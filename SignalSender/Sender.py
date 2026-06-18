418217ad26436a1fef77f54d189d15d35ab75521
Chosen-Juan1 <juan.guadalupe@upr.edu>
2026-06-18 16:54:12 -0400

    Made it so receiver and sender are now eternally sending packets to each other (until the contianers are shut down) and added a thrid container called man to act as the man in the middle, which can snoop the other's packets successfully. (Man needs to be in the network namespace of either receiver or sender)

--- SignalSender/Sender.py
+++ SignalSender/Sender.py
@@ -1,29 +1,58 @@
-from scapy.all import IP, UDP, Raw, send, sr1
+from scapy.all import IP, UDP, Raw, send, sr1, sniff, AsyncSniffer
 from Attacks.Fuzzing import Fuzzing
-import time
+from time import sleep
+def handle_packet(packet):
+    print("=== Packet received ===", flush=True)
+    packet.show2()
+    sleep(5)
+    print("=== Sending Response ===")
+    sr1(IP(dst="receiver") / UDP(sport=4000, dport=packet[UDP].sport) / Raw(load=b"Yuh uh"))
 
 # for x in range(0, 10):
 #     #imma attmept to send the packet 10 times every 5 seconds
 #     send(IP(dst="receiver") / UDP(dport=5000) / Raw(load="Hello receiver"))
 
 # send(IP(dst="receiver") / UDP(sport=4000, dport=5000) / Raw(load=b"Hello receiver"))
-time.sleep(5)
+packet = (IP(dst="receiver") / UDP(sport=4000, dport=5000) / Raw(load=b"Waffles are better than pancakes!!!"))
 
-packet = (IP(dst="receiver") / UDP(sport=4000, dport=5000) / Raw(load=b"Good morning, receiver :)"))
+    # packet = (IP(dst="127.0.0.1") / UDP(sport=4000, dport=5000) / Raw(load=b"Good morning, receiver :)"))
 
-# packet = (IP(dst="127.0.0.1") / UDP(sport=4000, dport=5000) / Raw(load=b"Good morning, receiver :)"))
+ans = sr1(packet)
+sniffer = AsyncSniffer(
+    iface="eth0",
+    prn=handle_packet,
+    store=False,
+    filter="udp dst port 4000"
+)
 
-sr1(packet)
-print("Packet sent")
-packet[Raw].load = b"Bad morning, receiver >:)"
-badPacket = packet
+sniffer.start()
+
+try:
+    while True:
+        print("Still sniffing...", flush=True)
+        sleep(5)
+except KeyboardInterrupt:
+    print("Stopping sniffer...", flush=True)
+    sniffer.stop()
+#<- apperantly this needs a specific linux lib, added it to the docker file
+
+# print("Packet sent")
+# packet[Raw].load = b"Bad morning from mac, receiver >:)"
+# badPacket = packet
 
 #Fuzzing test:
 
-badPacket = Fuzzing(badPacket)
-print("Modified packet sent")
+# badPacket = Fuzzing(badPacket)
+# print("Modified packet sent")
 
 # sr1(badPacket)
+
+    # if(Raw in packet):
+    #     print(f"Information sent with flush: {packet[Raw].load}\n", flush=True)
+
+
+
+
 
 
 
--- TheManInTheMiddle/Man.py
+++ TheManInTheMiddle/Man.py
@@ -1,35 +1,39 @@
-import time
+import os
+import signal
+import sys
 
-from scapy.all import sniff, IP, UDP, Raw, AsyncSniffer, sr1
+
+from scapy.all import IP, UDP, Raw, AsyncSniffer
 from time import sleep
 
 def handle_packet(packet):
-    print("=== Packet received ===", flush=True)
+    print("=== Packet Snooped ===", flush=True)
     packet.show2()
-    sleep(5)
-    print("=== Sending Response ===")
-    packet = (IP(dst="sender") / UDP(sport=5000, dport=packet[UDP].sport) / Raw(load=b"Nuh uh"))
-    sr1(packet)
-
     # if(Raw in packet):
     #     print(f"Information sent with flush: {packet[Raw].load}\n", flush=True)
+
+
 
 
 sniffer = AsyncSniffer(
     iface="eth0",
     prn=handle_packet,
     store=False,
-    filter="udp dst port 5000"
+    filter="udp and (port 5000 or port 4000)"
 )
 
 sniffer.start()
+
+# if KeyboardInterrupt:
+#     print("Stopping sniffer...", flush=True)
+#     sniffer.stop()
 
 try:
     while True:
         print("Still sniffing...", flush=True)
         sleep(5)
 except KeyboardInterrupt:
-    print("Stopping sniffer...", flush=True)
-    sniffer.stop()
+        print("Stopping sniffer...", flush=True)
+        sniffer.stop()
 
 
